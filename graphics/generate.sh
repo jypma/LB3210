df2c460f73d005f30dfdf7cfc12ea2b40c88cbd6
Jan Ypma <jan@ypmania.nl>
2021-06-03 09:35:34 +0200

    Updates for day 2
    - Textual changes
    - Add dependencies for second code exercise
    - Reorder slides to align with plan

--- graphics/generate.sh
+++ graphics/generate.sh
@@ -2,9 +2,14 @@
 # Exports all SVG files to PNG
 set -e
 
-DPI=192
+DPI=96
 
 for F in svg/*.svg
 do
     inkscape -d $DPI --export-type="png" $F
+done
+
+for F in bitmap/*
+do
+    mogrify -path . -resize "1200x700" $F
 done
